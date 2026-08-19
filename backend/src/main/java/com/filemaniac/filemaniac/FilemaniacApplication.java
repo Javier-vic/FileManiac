93675b16b95bf492c4d52a11adc3cf9bc90a1e1b
Javier Victoriano <93997406+Javier-vic@users.noreply.github.com>
2026-08-18 21:34:42 -0400

    chore: initialize springboot project structure

---
+++ backend/src/main/java/com/filemaniac/filemaniac/FilemaniacApplication.java
@@ -0,0 +1,13 @@
+package com.filemaniac.filemaniac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilemaniacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilemaniacApplication.class, args);
+	}
+
+}
